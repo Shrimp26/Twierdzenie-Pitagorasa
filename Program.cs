500dbe2b45da0229abbe9ea3fd710b72d738d91d
julia <julia@LAPTOP-ILMLJSM->
2024-02-08 15:31:23 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program{
+//Zaimplementuj twierdzenie Pitagorasa a^2+b^2=c^2. Oblicz długość przeciwprostokątnej trójkąta prostokątnego o przyprostokątnych a = 3.5 i b = 6. Wynik obliczeń wyświetl w konsoli.
+    static void Main()
+    {
+        double a = 3.5;
+        double b = 6;
+        double c = CalculateHypotenause(a, b);
+
+        Console.WriteLine($"Długość przeciwprostokątnej c = ; {c}");
+    }
+
+    static double
+        CalculateHypotenause (double a, double b)
+    {
+        return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+    }
+}
